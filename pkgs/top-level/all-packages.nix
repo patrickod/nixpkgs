413f058185c391e2a0bd3086f272275e49aa01bb
Patrick O'Doherty <p@trickod.com>
2020-07-24 18:18:21 -0700

    Add vfio-isolate CLI utility
    vfio-isolate is a Python utility that for facilitating CPU and memory isolation
    for running virtual machines with lower latency guarantees.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27163,4 +27163,6 @@ in
   gpio-utils = callPackage ../os-specific/linux/kernel/gpio-utils.nix { };
 
   navidrome = callPackage ../servers/misc/navidrome {};
+
+  vfio-isolate = callPackage ../applications/virtualization/vfio-isolate {};
 }
